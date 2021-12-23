10022177c00c09a81be83c451674e7c94310ea1d
Kruthika R <65350577+Kruthika-ML@users.noreply.github.com>
2021-12-23 19:06:54 +0530

    Add files via upload

---
+++ Class1.java
@@ -0,0 +1,12 @@
+public class Class1 {
+    int subjects;
+    int colour;
+    int name;
+    int engine;
+
+    public static void main(String[] args) {
+        Class1 love = new Class1();
+        love.name = 2;
+        System.out.println(love.name);
+    }
+}
