973d778e10659dd05377388b81a71471c97bbb8c
Kruthika R <65350577+Kruthika-ML@users.noreply.github.com>
2021-12-24 18:45:11 +0530

    Add files via upload

---
+++ country.java
@@ -0,0 +1,12 @@
+public class country {
+    int number;
+    String name;
+
+    public static void main(String[] args) {
+        country love = new country();
+        love.name = "Wayne";
+        love.number = 4;
+        System.out.println(love.name);
+        System.out.println(love.number);
+    }
+}
