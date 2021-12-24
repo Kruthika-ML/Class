9f7bd9135849724f6fadcdb8ca05b2ef7694636a
Kruthika R <65350577+Kruthika-ML@users.noreply.github.com>
2021-12-24 18:33:03 +0530

    Add files via upload

---
+++ person.java
@@ -0,0 +1,13 @@
+public class person {
+    int age;
+    String name;
+    String company;
+
+    public static void main(String[] args) {
+        person love = new person();
+        love.age = 22;
+        love.name = "Money";
+        System.out.println(love.name);
+        System.out.println(love.age);
+    }
+}
