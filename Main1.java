c4add584bec9e1d105b519a13d7be1d4eb6dcd5c
Kruthika R <65350577+Kruthika-ML@users.noreply.github.com>
2021-12-26 18:24:36 +0530

    Add files via upload

---
+++ Main1.java
@@ -0,0 +1,10 @@
+
+public class Main1 {
+    static void myMethod() {
+        System.out.println("Hello World!");
+    }
+
+    public static void main(String[] args) {
+        myMethod();
+    }
+}
